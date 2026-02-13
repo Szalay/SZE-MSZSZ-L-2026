c95723276ac109e3517c4b8bcdaa85693a9c59bb
Szalay <ifj.szalay.istvan@gmail.com>
2026-02-13 16:09:01 +0100

    Az ingamodell súrlódás nélkül.

---
+++ Pendulum.m
@@ -0,0 +1,91 @@
+classdef Pendulum < handle
+	
+	properties
+		% Modellparaméterek
+		m = 1;
+		L = 1;
+
+		% Kezdeti értékek
+		omega_0 = 0;
+		phi_0 = 0;
+
+		% A megoldáshoz tartozó mennyiségek
+		t;
+		x;	% [omega; phi]
+	end
+
+	properties (Constant)
+		g = 9.81;
+	end
+
+	properties (Dependent)
+		J;
+		x_0;
+	end
+
+	methods 
+		
+		function this = Pendulum(t_0, t_1)
+			this.t = [t_0, t_1];
+		end
+
+		function J = get.J(this)
+			J = this.m*this.L^2;
+		end
+
+		function x_0 = get.x_0(this)
+			x_0 = [this.omega_0; this.phi_0];
+		end
+
+		function dxdt = Model(this, ~, x)
+			% x = [omega; phi]
+			omega = x(1);
+			phi = x(2);
+
+			domegadt = -this.m*Pendulum.g*this.L/this.J * sin(phi);
+			dphidt = omega;
+
+			dxdt = [domegadt; dphidt];
+		end
+
+		function Simulate(this)
+			[this.t, this.x] = ode45(@this.Model, this.t, this.x_0);
+		end
+
+		function Plot(this)
+			window = figure();
+			window.Color = [1, 1, 1];
+			
+			% Szögsebesség
+			subplot(2, 1, 1); hold on; grid on; box on;
+			title("Szögsebesség", FontSize=18);
+			xlabel("Idő, t, (s)", FontSize=14);
+			ylabel("Szögsebesség, \omega, (rad/s)", FontSize=14);
+
+			plot(this.t, this.x(:, 1), "r-", LineWidth=3);
+			
+			% Szöghelyzet
+			subplot(2, 1, 2); hold on; grid on; box on;
+			title("Szöghelyzet", FontSize=18);
+			xlabel("Idő, t, (s)", FontSize=14);
+			ylabel("Szöghelyzet, \phi, (rad)", FontSize=14);
+
+			plot(this.t, this.x(:, 2), "b-", LineWidth=3);
+		end
+
+	end
+
+	methods (Static)
+		
+		function Run()
+			p = Pendulum(0, 10);
+
+			p.omega_0 = 1;
+
+			p.Simulate();
+			p.Plot();
+		end
+
+	end
+
+end
